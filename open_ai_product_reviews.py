b6908dfd513403aef3345c6b1fdc1e8be9db2d5d
lifeisbeautiful247 <125408558+lifeisbeautiful247@users.noreply.github.com>
2023-06-01 15:35:21 +1200

    Update open_ai_product_reviews.py

--- open_ai_product_reviews.py
+++ open_ai_product_reviews.py
@@ -1,7 +1,7 @@
 import openai
 import mysql.connector
 
-# Step 3: Connect to your MySQL database
+# Connect to your MySQL database
 db_connection = mysql.connector.connect(
     host='localhost',
     user='username',
@@ -9,16 +9,16 @@ db_connection = mysql.connector.connect(
     database='reviews'
 )
 
-# Step 4: Query the database to retrieve the product reviews
+# Query the database to retrieve the product reviews
 db_cursor = db_connection.cursor()
 db_cursor.execute("SELECT * FROM product_reviews")
 reviews = db_cursor.fetchall()
 
-# Step 5: Set up your OpenAI API credentials
+# Set up your OpenAI API credentials
 openai.api_key = 'API KEY'
 
 
-# Step 6: Define a function to send the user query to ChatGPT and get the response
+# Define a function to send the user query to ChatGPT and get the response
 def get_chatgpt_response(query):
     response = openai.Completion.create(
         engine="davinci",
@@ -29,7 +29,7 @@ def get_chatgpt_response(query):
     return response.choices[0].text.strip()
 
 
-# Step 7: Interactive loop
+# Interaction loop
 print("Welcome! Ask your questions about the app reviews!")
 while True:
     user_input = input("User: ")
